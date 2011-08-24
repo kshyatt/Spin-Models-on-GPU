6c6cda184b0a7a1d3f72b578d70aba915607c751
Katie Hyatt <kslimes@gmail.com>
2011-08-23 20:38:55 -0400

    Deleted a bunch of extraneous stuff

--- converter.h
+++
@@ -1,7 +0,0 @@
-#include<vector>
-#include"GenHam.h"
-#include"cuComplex.h"
-
-using namespace std;
-
-void converter(vector< vector<long> >, vector< vector<h_float> >, long* , long* , long*, cuDoubleComplex*);
--- tqli.h
+++
@@ -1,8 +0,0 @@
-// header for tqli.c
-#include<stdio.h>
-#include<math.h>
-#define SIGN(a,b) ((b) >= 0.0 ? fabs(a) : -fabs(a))
-
-int tqli(double* d, double* e, int n, double* z);
-
-double pythag(double a, double b);
